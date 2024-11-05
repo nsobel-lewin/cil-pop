859cf4dfd480660193cfa20eb98fa409f2c5dd5c
nsobel-lewin <nvsl@uchicago.edu>
2024-11-05 16:42:46 -0600

    Add maps

--- src/validate_ir_populations_fns.R
+++ src/validate_ir_populations_fns.R
@@ -23,6 +23,7 @@ make_pop_diff_density_plot <- function(datadir, product, year) {
     scale_y_continuous(
       expand = c(5e-6, 5e-6)
     ) +
+    labs(title = str_c(str_to_sentence(product), " - ", year)) +
     theme_classic() +
     theme(axis.title.y = element_blank())
 
@@ -53,6 +54,7 @@ make_rescale_factor_density_plot <- function(datadir, product, year) {
       limits = c(0.8, 1.6),
       oob = scales::squish
     ) +
+    labs(title = str_c(str_to_sentence(product), " - ", year)) +
     theme_classic() +
     theme(axis.title.y = element_blank())
   
@@ -69,20 +71,27 @@ make_pop_plot <- function(datadir, simplified_shp, product, year, rescaled) {
   
   plot_data <- simplified_shp %>% 
     full_join(ir_pop, by = "gadmid") %>% 
+    # Drop Antarctica
+    filter(gadmid != 2836) %>% 
     select(-gadmid) %>% 
-    mutate(pop = if_else(pop == 0, NA, log10(pop)))
+    mutate(pop = if_else(pop == 0, -1, log10(pop)))
   
   plot <- ggplot(plot_data, aes(fill = pop)) +
     geom_sf(linewidth = 0) +
     scale_fill_viridis_c(
       begin = 0.05, 
-      end = 1,
+      end = 0.98,
       breaks = c(3, 4, 5, 6),
       labels = c("<1,000", "10,000", "100,000", ">1,000,000"),
       limits = c(3, 6),
       oob = scales::squish) +
-    theme_classic() +
-    theme(axis.title.y = element_blank())
+    labs(title = str_c(str_to_sentence(product), " - ", year, 
+                       if_else(rescaled, " - rescaled", ""))) +
+    theme_void() +
+    theme(axis.title.y = element_blank(),
+          legend.title = element_blank(),
+          legend.key.width = unit(2.5, "cm"),
+          legend.position = "bottom") 
   
   return(plot)
 }
